3426672a3313b1fa2e1f9d4142c8facc496021fd
harshithprasad <harshith@hotelsimply.com>
2018-02-28 19:08:11 +0530

    intial theme

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -10,6 +10,19 @@
 // Read Sprockets README (https://github.com/rails/sprockets#sprockets-directives) for details
 // about supported directives.
 //
+
+//= require jquery.2.2.3.min
+//= require bootstrap.min
+//= require jarallax.min
+//= require jquery.sidebar.min
+//= require owl.carousel.min
+//= require jquery.magnific-popup.min
+//= require jquery.counterup
+//= require waypoints.min
+//= require mCustomScrollbar.min
+//= require waves.min
+//= require gmaps
+//= require custom
 //= require rails-ujs
 //= require turbolinks
 //= require_tree .
